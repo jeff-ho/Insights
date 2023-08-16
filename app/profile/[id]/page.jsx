594a14eb65c1290f6b3df7624672af29d11330fe
Jeff <jeff.ho@hotmail.com>
2023-08-16 14:42:26 -0400

    Implemented viewing of other user profiles

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+
+import Profile from "@components/profile";
+
+const UserProfile = ({ params }) => {
+  const searchParams = useSearchParams();
+  const userName = searchParams.get("name");
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${params?.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+    if (params?.id) fetchPosts();
+  }, []);
+
+  return (
+    <section>
+      <Profile
+        name={userName}
+        desc={`Welcome to ${userName}'s profile page.`}
+        data={posts}
+      ></Profile>
+    </section>
+  );
+};
+
+export default UserProfile;
